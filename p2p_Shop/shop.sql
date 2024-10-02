bc3a88129b7e85866ca398f5d836491a2703f6e4
Jul0P <180294787+Jul0P@users.noreply.github.com>
2024-10-02 22:10:32 +0200

    add: folder scripts

--- p2p_Shop/shop.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-    ('bread', "Pain", 1, 0, 1),
-    ('sandwich', "Sandwich", 1, 0, 1),
-    ('water', "Bouteille d'Eau", 1, 0, 1),
-    ('jorange', "Jus d'Orange", 1, 0, 1),
-;
