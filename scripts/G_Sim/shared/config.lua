694c72ab64f91886a4e654de53d832e519600034
Jul0P <jules.parents@gmail.com>
2024-11-21 20:10:58 +0100

    add: Sim

---
+++ scripts/G_Sim/shared/config.lua
@@ -0,0 +1,6 @@
+G_Sim = {
+    Key = "F2",
+    Command = "opensimmenu",
+    Language = "US", -- # US or FR
+    GKSPHONE = false, -- # Si vous avez un autre téléphone laissez sur false, si vous avez le gksphone par contre mettez true
+}
